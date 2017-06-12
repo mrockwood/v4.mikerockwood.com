44a6bc6f7402dacd1abcc3f273a38eaf8d75b5f4
mrockwood <mike@dhcp-155-99-82-195.campus.utah.edu>
2017-06-12 13:52:56 -0600

    move all JS into main.js

--- _assets/scripts/main.js
+++ _assets/scripts/main.js
@@ -1,3 +1,9 @@
+/* ==========================================================================
+   #MAIN
+   ========================================================================== */
+
+
+
 
 /**
  * Load svg4everybody
@@ -15,5 +21,29 @@ svg4everybody();
 $(document).ready(function() {
 	$('.js-zoom').click(function() {
 		$(this).toggleClass('c-zoom?full-size');
+	});
+});
+
+
+
+
+/**
+ * Load Disqus comments on button click
+ */
+
+$(document).ready(function() {
+	$('.js-load-comments').on('click', function(){
+		var disqus_shortname = 'YOUR-DISQUS-USERNAME'; // Replace this value with *your* username.
+
+		// ajax request to load the disqus javascript
+		$.ajax({
+			type: "GET",
+			url: "https://" + disqus_shortname + ".disqus.com/embed.js",
+			dataType: "script",
+			cache: true
+		});
+
+		// hide the button once comments load
+		$(this).fadeOut();
 	});
 });
